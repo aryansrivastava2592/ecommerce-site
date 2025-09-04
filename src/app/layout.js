e52589288933cca3035d4b76c6557241925692dc
Aryan Srivastava <aryan250902ars@gmail.com>
2025-09-05 02:34:31 +0530

    feat: Brand site to RupeeRack

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,19 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { StoreProvider } from '@/context/Store'
-import Link from 'next/link'
-import Menu from '@/components/Menu'
-import SessionWrapper from '@/components/SessionWrapper'
-import SearchBox from '@/components/SearchBox'
-import CategoryNav from '@/components/CategoryNav'
-import { Toaster } from 'react-hot-toast'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { StoreProvider } from "@/context/Store";
+import Link from "next/link";
+import Menu from "@/components/Menu";
+import SessionWrapper from "@/components/SessionWrapper";
+import SearchBox from "@/components/SearchBox";
+import CategoryNav from "@/components/CategoryNav";
+import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'My E-Commerce App',
-  description: 'A modern e-commerce platform built with Next.js',
-}
+  title: "RupeeRack",
+  description: "RupeeRack - Your One-Stop Online Shop",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
                 <nav className="relative flex h-16 items-center px-6 justify-between shadow-md z-20">
                   <div className="flex items-center">
                     <Link href="/" className="text-lg font-bold">
-                      E-Commerce
+                      RupeeRack
                     </Link>
                   </div>
                   <div className="hidden md:block flex-grow px-8">
@@ -46,5 +46,5 @@ export default function RootLayout({ children }) {
         </SessionWrapper>
       </body>
     </html>
-  )
+  );
 }
