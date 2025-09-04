be1b3f4885243ad53e8e93aa1c2d0cd691e71f8c
Aryan Srivastava <aryan250902ars@gmail.com>
2025-09-05 02:47:59 +0530

    feat: copyright to RupeeRack

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
               </header>
               <main className="container m-auto mt-4 px-4">{children}</main>
               <footer className="flex h-10 justify-center items-center shadow-inner">
-                <p>Copyright © 2025 E-Commerce</p>
+                <p>Copyright © 2025 RupeeRack</p>
               </footer>
             </div>
           </StoreProvider>
